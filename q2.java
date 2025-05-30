99a533eaec982fff6356e305c1d7bbaefaed6c50
SHIFANA N <shifanasportal@gmail.com>
2025-05-30 16:01:03 +0530

    Add files via upload

---
+++ q2.java
@@ -0,0 +1,13 @@
+import java.lang.System;
+import java.util.Scanner;
+class hello{
+    public static void main(String args[])
+    {
+int a = 3;
+int b = 30;
+int c = 2;
+int d = (a*b*c);
+int e = (a+b+c);
+System.out.println(d%e);
+    }
+}
