7d91d9d62fc876c31f3369936574225fe4a58ad7
SHIFANA N <shifanasportal@gmail.com>
2025-06-05 15:59:33 +0530

    Add files via upload

---
+++ q4.java
@@ -0,0 +1,13 @@
+class hello
+{
+    public static void main(String args[])
+    {
+        if (true)
+{
+    System.out.println("yessssss");
+}
+else{
+    System.out.println("noooooo");
+}
+    }
+}
---
+++ q5.java
@@ -0,0 +1,10 @@
+
+class hello
+{
+    public static void main(String args[])
+    {
+        System.out.println(3>8);
+         System.out.println(11>8);
+    }
+}
+
