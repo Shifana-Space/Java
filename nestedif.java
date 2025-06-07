f92b2b12ecf7aea1d8c962c38c25c55ea66da1be
SHIFANA N <shifanasportal@gmail.com>
2025-06-07 21:07:24 +0530

    Add files via upload

---
+++ nestedif.java
@@ -0,0 +1,21 @@
+class hello{
+public static void main(String args[])
+{
+    Boolean kfc=false;
+    Boolean chicken=true;
+  Boolean pepsi=false;
+    if (kfc)
+    {  
+        System.out.println("Enter into kfc");
+    }
+if (chicken)
+    {  
+        System.out.println("eating chicken");
+    }
+if (pepsi)
+    {  
+        System.out.println("Drinking pepsi");
+    }
+
+}
+}
