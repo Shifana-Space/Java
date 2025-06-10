e60b66769712840912df11f870d59e0e2bccfa68
SHIFANA N <shifanasportal@gmail.com>
2025-06-10 19:25:50 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class hello{
+    public static void main(String args[])
+    {
+        int[] playlist = new int[2] ;
+        Scanner scan = new Scanner(System.in);
+        playlist[0]=scan.nextInt();
+         playlist[1]=scan.nextInt();
+         int a = playlist[0]+playlist[1];
+         System.out.println(a);
+}
+    }
+    
