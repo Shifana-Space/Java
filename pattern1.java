b361cf3a0fd5069a8251c09423f57bcaed02c6df
SHIFANA N <shifanasportal@gmail.com>
2025-06-11 21:19:05 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,10 @@
+class hello{
+    public static void main(String args[])
+    {
+for (int j=1;j<=10;j=j+1){
+    for(int i=1;i<=j;i=i+1){
+
+System.out.print("*");
+    } System.out.println();
+    }}
+    }
