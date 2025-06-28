c89e8ca4597e4667855a34af1cafcdf0a5a3b635
SHIFANA N <shifanasportal@gmail.com>
2025-06-28 19:05:11 +0530

    Add files via upload

---
+++ 2Darray.java
@@ -0,0 +1,11 @@
+class 2Darray{
+    public static void main(String args[]){
+        int[][] arr = {{4,8},{9,5},{7,2}};
+          for(int row =0;row<arr.length;row++){
+            for(int col=0;col<arr[1].length;col++)
+            {
+                System.out.print(arr[row][col]+" ");}
+
+            System.out.println( );
+          }
+          }}
